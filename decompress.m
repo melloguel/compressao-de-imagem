324366c09ce6cc0ebb50dce4eb6c4fa130a60b50
Miguel Mello Carpi <miguel.carpi23@gmail.com>
2020-05-28 19:05:24 -0300

    Bilinear tá lisinha

--- decompress.m
+++ decompress.m
@@ -4,6 +4,7 @@ function decompress(compressedImg, method, k, h)
   [imgRead,imgMap] = imread(compressedImg);
   imgRead = im2double(imgRead);
   r = rows(imgRead);
+  c = columns(imgRead);
   tamanho = r + (r - 1)*k;
   decImg = zeros(tamanho, tamanho, 3); # matriz de zeros de formato tam*tam*3
 
@@ -14,8 +15,8 @@ function decompress(compressedImg, method, k, h)
   if (method == 1) # Interpolação bilinear por partes
 
       for i = 1:r
+	ii = (k+1) * (i - 1) + 1;
 	for j = 1:r
-	  ii = (k+1) * (i - 1) + 1;
 	  jj = (k+1) * (j - 1) + 1;
 	  decImg(ii, jj, 1:3) = imgRead(i, j, 1:3);
 	endfor
@@ -27,44 +28,40 @@ function decompress(compressedImg, method, k, h)
 	       [1, h, h, h*h]];
     invH = inv(matrizH);
 
+    r = rows(decImg);
+    c = columns(decImg);
     
     
-    for i = k+2:k+1:rows(decImg)
-      for j = 1:k+1:rows(decImg) - k - 1
+    for i = 1:(k+1):(r-(k+1))
+      for j = 1:(k+1):(c-(k+1))
 
 	matrizF1 = [decImg(i, j, 1);	 
 		    decImg(i, j+k+1, 1);	 
-		    decImg(i-k-1, j, 1);	 
-		    decImg(i-k-1, j+k+1, 1)];
+		    decImg(i+k+1, j, 1);	 
+		    decImg(i+k+1, j+k+1, 1)];
 	coef1 = invH * matrizF1;
 
 	matrizF2 =  [decImg(i, j, 2);	 
 		     decImg(i, j+k+1, 2);	 
-		     decImg(i-k-1, j, 2);	 
-		    decImg(i-k-1, j+k+1, 2)];
+		     decImg(i+k+1, j, 2);	 
+		    decImg(i+k+1, j+k+1, 2)];
 	coef2 = invH * matrizF2;
 
 	matrizF3 =  [decImg(i, j, 3);	 
 		     decImg(i, j+k+1, 3);	 
-		     decImg(i-k-1, j, 3);	 
-		     decImg(i-k-1, j+k+1, 3)];
+		     decImg(i+k+1, j, 3);	 
+		     decImg(i+k+1, j+k+1, 3)];
 	coef3 = invH * matrizF3;	
 
-
-	xc = h;
-	for x = 1:k+1
-	  xc  -= (h/(k+1));
+	xc = 0;
+	for x = 0:k+1
+	  xc = x*(h/(k+1));
 	  yc = 0;
-	  for y = 1:k+1
-	    yc += (h/(k+1));
-	    if (i == x)
-	      var = 1;
-	    else
-	      var = i - x;
-	    endif	    
-	    decImg(var, j+y, 1) = coef1(1) + coef1(2) * xc + coef1(3) * yc + coef1(4) * xc * yc;
-	    decImg(var, j+y, 2) = coef2(1) + coef2(2) * xc + coef2(3) * yc + coef2(4) * xc * yc;
-	    decImg(var, j+y, 3) = coef3(1) + coef3(2) * xc + coef3(3) * yc + coef3(4) * xc * yc;	    
+	  for y = 0:k+1	    
+	    yc = y*(h/(k+1));
+	    decImg(i+x, j+y, 1) = coef1(1) + coef1(2) * xc + coef1(3) * yc + coef1(4) * xc * yc;
+	    decImg(i+x, j+y, 2) = coef2(1) + coef2(2) * xc + coef2(3) * yc + coef2(4) * xc * yc;
+	    decImg(i+x, j+y, 3) = coef3(1) + coef3(2) * xc + coef3(3) * yc + coef3(4) * xc * yc;	    
 	  endfor
 	endfor
       endfor
